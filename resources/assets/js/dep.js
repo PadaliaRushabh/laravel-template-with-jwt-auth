67756a7bd08902beda47a3ea6d0a11241449088c
Rushabh Padalia <padalia.rushabh@gmail.com>
2017-07-02 14:59:45 +1000

    lodash and axios plugin

--- resources/assets/js/dep.js
+++ resources/assets/js/dep.js
@@ -4,3 +4,5 @@ window.confirm = require('jquery-confirm');
 window.bootstrap = require('bootstrap');
 window.collect = require('collect.js');
 window.moment = require('moment');
+window.axios = require('axios');
+window._ = require('lodash');
